bc782c848d1f7516a042690f1888f486a26abd5d
Mathias Beckius <lillmatte@hotmail.com>
2020-01-25 20:24:50 +0100

    moved session_start again

--- includes/checklogin.php
+++ includes/checklogin.php
@@ -1,12 +1,12 @@
 <?php
 include_once('config.php');
-session_start();
 if (isset($_POST["username"]) && isset($_POST["password"])) {
     $postUsername = filter_input(INPUT_POST, 'username');
     $postPassword = filter_input(INPUT_POST, 'password');
     $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
     $stmt->execute([$postUsername]);
     $row = $stmt->fetch();
+    session_start();
     if ($stmt->rowCount() > 0) {
         if (password_verify($postPassword, $row['password'])) {
             $_SESSION["username"] = $row["username"];

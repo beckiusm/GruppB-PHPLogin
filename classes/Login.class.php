d5b2e37e313418c85311a37cc1bf3fe8ae295a08
thelopster <nitipon.apaisri@gmail.com>
2020-01-27 11:12:13 +0100

    stuck

--- classes/Login.class.php
+++ classes/Login.class.php
@@ -6,26 +6,39 @@ class Login
     private $password = "";
     private $email = "";
     
-}
-public function __construct($username, $password, $password)
+    public function __construct($username, $password, $password)
+    
+    {
+        require_once __DIR__ . '/../vendor/autoload.php';
+    
+            $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+            $dotenv->load();
+        $this->host = getenv("DB_ADDRESS");
+            $this->port = getenv("DB_PORT");
+            $this->db = getenv("DB_NAME");
+            $this->user = getenv("DB_USER");
+            $this->pass = getenv("DB_PASSWORD");
+    
+            $this->dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->db;charset=$this->charset";
+            $this->options = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
+    
+            try {
+                $this->pdo = new PDO($this->dsn, $this->user, $this->pass, $this->options);
+            } catch (\PDOException $e) {
+                throw new \PDOException($e->getMessage(), (int) $e->getCode());
+            }
+    }
 
-{
-    $this->host = getenv("DB_ADDRESS");
-        $this->port = getenv("DB_PORT");
-        $this->db = getenv("DB_NAME");
-        $this->user = getenv("DB_USER");
-        $this->pass = getenv("DB_PASSWORD");
+    session_start();
+    
+    public function ifUserExits($username)
+    {
+        $stmt = $this->pdo->prepare("SELECT username FROM users WHERE username = ?");
 
-        $this->dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->db;charset=$this->charset";
-        $this->options = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-
-        try {
-            $this->pdo = new PDO($this->dsn, $this->user, $this->pass, $this->options);
-        } catch (\PDOException $e) {
-            throw new \PDOException($e->getMessage(), (int) $e->getCode());
-        }
+    }
+    
 }

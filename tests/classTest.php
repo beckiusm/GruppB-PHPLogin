5e214e180e300d2846db88c3a226c02f89d6917e
Mathias Beckius <lillmatte@hotmail.com>
2020-01-27 23:07:25 +0100

    more tests

--- tests/classTest.php
+++ tests/classTest.php
@@ -1,15 +1,33 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+
 include_once("./classes/Register.class.php");
+include_once("./classes/DB.class.php");
 
 class registerTest extends TestCase
 {
+    public $username = "TestUser";
+    public $password = "123";
+    public $email = "123@hotmail.com";
+
     public function testLoginIsObject()
     {
-        $username = "TestUser";
-        $password = "123";
-        $email = "123@hotmail.com";
-        $this->assertIsObject(new Register($username, $password, $email));
+        $register = new Register($this->username, $this->password, $this->email);
+        $this->assertIsObject($register);
+        $this->assertIsObject(new DB());
+    }
+
+    public function testClassHasAttribute()
+    {
+        $this->assertClassHasAttribute("email", Register::class);
+        $this->assertClassHasAttribute("username", Register::class);
+        $this->assertClassHasAttribute("password", Register::class);
+    }
+
+    public function testHashMethod()
+    {
+        $hashedPw = password_hash($this->password, PASSWORD_DEFAULT);
+        $this->assertTrue(password_verify($this->password, $hashedPw));
     }
 }
